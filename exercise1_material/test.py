ce13672cf7de1ce8b2fd5f994dc5a24380c6deb5
thesebsterseb <sebastian.zimmermann@fau.de>
2024-05-13 17:28:09 +0200

    Added test.py file which is just a playground file for me

--- exercise1_material/test.py
+++ exercise1_material/test.py
@@ -1,14 +1,17 @@
 import numpy as np
 
+# Just a testfile for myself to get some things debugged
 [1,2,3,4]
 
 # 4 Samples, 20 features each
 input_tensor = np.random.uniform(0,1,(4,20))
 
-ones = np.ones(shape=(input_tensor.shape[0],1))
-
+#ones = np.ones(shape=(input_tensor.shape[0],1))
+ones = np.ones((1, input_tensor.shape[0])).T
 print(ones.shape)
 
 fuckthisshit = np.hstack((input_tensor, ones))
+
+anothershit = np.concatenate((input_tensor, ones), axis=1)
 
 print(fuckthisshit.shape)
