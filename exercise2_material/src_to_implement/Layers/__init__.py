9cfe971f40366f4037641936ea30c3bf28db6c11
thesebsterseb <sebastian.zimmermann@fau.de>
2024-06-03 15:29:27 +0200

    Implemented the Flatten Layer, test runs successful

--- exercise2_material/src_to_implement/Layers/__init__.py
+++ exercise2_material/src_to_implement/Layers/__init__.py
@@ -1,4 +1,4 @@
 # Removed the not yet implemented Layers to get the Unit Tests working!
 #__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv", "Pooling", "Initializers", "Flatten", "Base"]
 
-__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Initializers", "Base"]
+__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Initializers", "Flatten","Base"]
