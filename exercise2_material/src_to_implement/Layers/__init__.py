f5c0216a37f9e42d7d88c08df4a9f1b47237b192
thesebsterseb <sebastian.zimmermann@fau.de>
2024-06-13 11:52:51 +0200

    minor changes

--- exercise2_material/src_to_implement/Layers/__init__.py
+++ exercise2_material/src_to_implement/Layers/__init__.py
@@ -1,5 +1,5 @@
 # Removed the not yet implemented Layers to get the Unit Tests working!
-__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv", "Pooling", "Initializers", "Flatten", "Base"]
+__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv_test", "Pooling", "Initializers", "Flatten", "Base"]
 
 #__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU","Conv", "Initializers", "Flatten","Base", "Pooling"]
 
