6667818470a52184bb6aa76a22236f2e00253d42
thesebsterseb <sebastian.zimmermann@fau.de>
2024-06-13 11:18:42 +0200

    Cross checking with some extra code

---
+++ exercise2_material/src_to_implement/Layers/Conv2.py
@@ -0,0 +1,23 @@
+from . import Base
+import numpy as np
+
+class Conv(Base.BaseLayer):
+    def __init__(self, stride_shape,convolution_shape, num_kernels):
+        super.__init__()
+        self.trainable = True
+        self.stride_shape = stride_shape
+        self.convolution_shape = convolution_shape
+        self.num_kernels = num_kernels
+            
+    # Notes:
+    # 1D-Input: Shape : [b,c,y] = batch, channel, spatial dimension
+    # 2D-Input: Shape : [b, c, y, x] = batch, channel, spatial dimension y, spatial dimension x
+    
+    def forward(self, input_tensor):
+        if len(input_tensor.shape) == 2:
+            one_dimentional_conv = True
+            #output_height = np.floor(input_tensor.shape[])
+
+        
+        
+        output_shape = (output_height)
--- exercise2_material/src_to_implement/Layers/__init__.py
+++ exercise2_material/src_to_implement/Layers/__init__.py
@@ -1,4 +1,5 @@
 # Removed the not yet implemented Layers to get the Unit Tests working!
-#__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv", "Pooling", "Initializers", "Flatten", "Base"]
+__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv", "Pooling", "Initializers", "Flatten", "Base"]
 
-__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Initializers", "Flatten","Base", "Pooling"]
+#__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU","Conv", "Initializers", "Flatten","Base", "Pooling"]
+
